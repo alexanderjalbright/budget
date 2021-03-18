b3074dce710cabc383a24402992f3541f7c2db70
jordywestside <mm1jwest@gmail.com>
2021-03-18 09:50:30 -0500

    submit button functioning

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import {
     SignIn,
     Profile,
 } from 'components/examples';
+import { Submit } from 'components';
 import Link from 'next/link';
 
+// This is what shows up on the page
 export default function Home({ names }) {
     return (
         <div>
@@ -19,14 +21,15 @@ export default function Home({ names }) {
                     Go to ServerSide Protected Page
                 </a>
             </Link>
+            <Submit data={'peepeepoopoo'} />
         </div>
     );
 }
 
+// This is a call to the API
 export async function getServerSideProps(context) {
     const res = await fetch(`${process.env.HOST}/api/creators`);
     const data = await res.json();
-
     if (!data) {
         return {
             notFound: true,
