71c981eca596bb75067fa4476abb354e76dfc473
Artie <anegroncsu@gmail.com>
2021-04-08 19:45:35 -0400

    Moved icons to seperate files.

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,55 +1,19 @@
+import AddIcon from './AddIcon';
+import GraphIcon from './GraphIcon';
+import HistoryIcon from './HistoryIcon';
 import styles from './NavBar.module.css';
 
 export default function NavBar() {
     return (
         <nav className={styles.navbar}>
             <a href="#">
-                <svg
-                    class="h-8 w-8 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                    />
-                </svg>
+                <GraphIcon />
             </a>
             <a href="#">
-                <svg
-                    class="h-8 w-8 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                </svg>
+                <AddIcon />
             </a>
             <a href="#">
-                <svg
-                    class="h-8 w-8 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                </svg>
+                <HistoryIcon />
             </a>
         </nav>
     );
